3ebfdff1234b0d3abcb8e5070226e8cfb6091b44
yhj <hyojun99222@gmail.com>
2022-12-08 16:27:45 +0900

    useSelect 최적화하기

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {useDispatch, useSelector} from "react-redux";
+import {shallowEqual, useDispatch, useSelector} from "react-redux";
 import {decrease, increase, setDiff} from "../modules/counter";
 import Counter from  "../components/Counter";
 
@@ -10,7 +10,14 @@ function CounterContainer() {
     const {number, diff} = useSelector(state => ({
         number: state.counter.number,
         diff: state.counter.diff
-    }));
+    }), shallowEqual);
+    // shallowEqual 을 두번째 인자로 전달해주면
+    // 이전 값과 다음 값을 비교하여 true인 경우 렌더링을 하지 않고 false인 경우 렌더링을 한다.
+    // shallowEqual 은 react - redux에 내장되어있는 함수로써, 객체 안의 가장 겉에 있는 값들을 모두 비교해준다.
+
+    //불필요한 렌더링 방지 방법2
+    // const number = useSelector(state => state.counter.number);
+    // const diff = useSelector(state => state.counter.diff);
 
     const dispatch = useDispatch();
     const onIncrease = () => dispatch(increase());
